6704a2574b08eec5dfb816a0724029b6c8c1427f
KyleBax <102880750+KyleBax@users.noreply.github.com>
2022-11-08 13:09:46 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizMaker
+{
+    internal class Program
+    {
+        public static readonly int NUMBER_OF_QUESTIONS = 2;
+
+        static void Main(string[] args)
+        {
+            UI.Intro();
+        }
+    }
+}
---
+++ UI.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizMaker
+{
+    internal class UI
+    {
+        public static void Intro()
+        {
+            Console.WriteLine(@"Welcome to THE QUIZMAKER!!!
+All you need to play are: Some questions, of which you know the answer to.
+A bit of creativity for some fake answers.
+And some friends to challenge (well not really, but I'm sure it's more fun with friends).
+Let's get started");
+        }
+    }
+}
